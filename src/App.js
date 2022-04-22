e09adc261a5081f54609abc53b08efc783f0ea35
Savage Coder <savagecoder352@gmail.com>
2022-04-22 10:32:19 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import {Fragment, useCallback, useEffect, useState} from "react";
+import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@mui/material";
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [filteredValue, setFilteredValue] = useState('');
+
+  const searchForUserHandler = (event) => {
+    let searchValue = event.target.value;
+    setFilteredValue(searchValue);
+  }
+
+  const filteredUsers = users.filter(user => {
+    return  user.name.toLowerCase().includes(filteredValue.toLowerCase());
+  });
+
+  const fetchUsersHandler = useCallback(async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
+
+      const allUsers = [];
+
+      for (const key in data) {
+        allUsers.push({
+          id: key,
+          name: data[key].name,
+          street: data[key].address.street,
+          suite: data[key].address.suite,
+          city: data[key].address.city,
+          zipcode: data[key].address.zipcode
+        });
+      }
+      setUsers(allUsers);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [setUsers]);
+
+  useEffect(() => {
+    fetchUsersHandler();
+  }, [fetchUsersHandler]);
+
+  let content = filteredUsers.map(user => {
+    return (
+        <Accordion key={user.id}>
+          <AccordionSummary
+              expandIcon={<ExpandMoreIcon/>}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+          >
+            <Typography>{user.name}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{user.street}</Typography>
+            <Typography>{user.suite}</Typography>
+            <Typography>{user.city}</Typography>
+            <Typography>{user.zipcode}</Typography>
+          </AccordionDetails>
+        </Accordion>
+    );
+  });
+
+  if (filteredUsers.length === 0) {
+    content = <p>No users found.</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Fragment>
+        <h1 className="header">ADP Users</h1>
+        <input className="filter-input" placeholder="Filter" onChange={searchForUserHandler}/>
+        {content}
+      </Fragment>
   );
 }
 
